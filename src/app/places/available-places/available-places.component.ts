501bb250feee511851b2c7a770c2bdca7b860f17
anjezzaa <anjeza.murati@gmail.com>
2026-01-27 14:55:45 +0100

    fetching places - fallback

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -15,11 +15,13 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
 })
 export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
   private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
 
   // constructor(private httpClient: HttpClient) {}
   ngOnInit() {
+    this.isFetching.set(true);
     const subscription = this.httpClient
     .get<{places: Place[] }>('http://localhost:3000/places')
     .pipe(
@@ -29,6 +31,9 @@ export class AvailablePlacesComponent implements OnInit {
       next: (places) => {
         this.places.set(places);
       },
+      complete:()=>{
+        this.isFetching.set(false);
+      }
     });
 
     this.destroyRef.onDestroy(()=> {
