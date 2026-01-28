b7163f10a43e2bd7e6dca841261d60a07ea47874
anjezzaa <anjeza.murati@gmail.com>
2026-01-28 16:00:13 +0100

    http interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { 
+    HttpEventType,
     HttpHandlerFn, 
     HttpRequest, 
     provideHttpClient, 
@@ -7,11 +8,30 @@ import {
 } from '@angular/common/http';
 
 import { AppComponent } from './app/app.component';
+import { tap } from 'rxjs';
 
-function loggingInterceptor(request: HttpRequest<unknown> , next: HttpHandlerFn ) {
+function loggingInterceptor(
+    request: HttpRequest<unknown> , 
+    next: HttpHandlerFn) 
+    {
+  
+    // const req = request.clone({
+    //     headers: request.headers.set('X-DEBUG', 'TESTING'),
+    // });
+
     console.log('[Outgoing Request]');
-    console.log(request)    
-    return next(request);
+    console.log(request);   
+    return next(request).pipe(
+        tap({
+            next: event => {
+                if (event.type === HttpEventType.Response) {
+                    console.log('[Incoming Response]');
+                    console.log(event.status);
+                    console.log(event.body);
+                }
+            }
+        })
+    );
 }
 
 
